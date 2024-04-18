86fbb695625a9828fe2bfe4782e8e8e0b14578c4
MRW Neundorf <ninevillage@gmail.com>
2024-04-18 13:20:42 +0200

    feat(cli): setup counter app example from ratatui (without error handling)

--- crates/dbv/src/main.rs
+++ crates/dbv/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+mod tui;
+mod app;
+
+fn main() -> io::Result<()> {
+    let mut terminal = tui::init()?;
+    let app_result = app::App::default().run(&mut terminal);
+    tui::restore()?;
+    app_result
 }
